7545d27d7c651fa5d37321cd4b09ba13c85a3e6d
Drew <andrew.christofferson97@gmail.com>
2021-02-11 13:41:02 -0700

    docker working with postgres

--- src/main/resources/db/migration/V1__UserTable.sql
+++ src/main/resources/db/migration/V1__UserTable.sql
@@ -1,8 +1,14 @@
---DROP EXTENSION 'uuid-ossp';
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id UUID NOT NULL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email varchar(100) NOT NULL
 );
 
+INSERT INTO users (id, name, email) VALUES
+(uuid_generate_v4(), 'drew', 'drew@gmail.com'),
+(uuid_generate_v4(), 'seth', 'seth@gmail.com'),
+(uuid_generate_v4(), 'oleg', 'oleg@gmail.com'),
+(uuid_generate_v4(), 'misha', 'misha@gmail.com'),
+(uuid_generate_v4(), 'ivan', 'ivan@gmail.com');
