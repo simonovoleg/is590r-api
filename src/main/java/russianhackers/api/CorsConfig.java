6237fb66a84c75d6c4c7a2bd5617c1e7445d5730
Oleg Simonov <simonov.vsk@gmail.com>
2021-04-15 18:26:49 -0600

    Relocated CorsConfig

--- src/main/java/russianhackers/api/CorsConfig.java
+++ src/main/java/russianhackers/api/CorsConfig.java
@@ -1,4 +1,4 @@
-package russianhackers.api.security;
+package russianhackers.api;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
