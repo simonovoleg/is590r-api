6822ca640f27284058594190d67fd7de16180d62
Oleg Simonov <simonov.vsk@gmail.com>
2021-04-16 23:35:07 -0600

    Added @JsonProperty annotation to Record and ApplicationUser models

--- src/main/java/russianhackers/api/model/ApplicationUser.java
+++ src/main/java/russianhackers/api/model/ApplicationUser.java
@@ -12,7 +12,10 @@ public class ApplicationUser {
     private final String name;
     private final String email;
 
-    public ApplicationUser(UUID user_id, @NotBlank String name, String email) {
+    public ApplicationUser(
+            @JsonProperty("user_id") UUID user_id,
+            @JsonProperty("name") @NotBlank String name,
+            @JsonProperty("email") String email) {
         this.user_id = user_id;
         this.name = name;
         this.email = email;
